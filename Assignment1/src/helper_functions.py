7a92b1e3da344e7af8878a94aa551fa35f2320fe
Maksim Karnaukh <s0201604@ua.ad.ac.be>
2024-03-11 05:06:52 +0100

    too much binning

--- Assignment1/src/helper_functions.py
+++ Assignment1/src/helper_functions.py
@@ -4,3 +4,8 @@ import pandas as pd
 def load_dataset(file_path):
     dataset = pd.read_excel(file_path)
     return dataset
+
+
+def save_dataset(dataset, file_path):
+    dataset.to_excel(file_path, index=False)
+    return
