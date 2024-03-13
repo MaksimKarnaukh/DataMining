e241bb7fc5bfd99634a8adf2d810f45e0e5e5a11
Maksim Karnaukh <s0201604@ua.ad.ac.be>
2024-03-13 01:59:10 +0100

    first file output

--- Assignment1/src/helper_functions.py
+++ Assignment1/src/helper_functions.py
@@ -2,10 +2,20 @@ import pandas as pd
 
 
 def load_dataset(file_path):
-    dataset = pd.read_excel(file_path)
+    try:
+        dataset = pd.read_excel(file_path)
+    except Exception as e:
+        print(e)
+        print("Error: Unable to load dataset")
+        return None
     return dataset
 
 
 def save_dataset(dataset, file_path):
-    dataset.to_excel(file_path, index=False)
+    try:
+        dataset.to_excel(file_path, index=False)
+    except Exception as e:
+        print(e)
+        print("Error: Unable to save dataset")
+        return
     return
